858de5c81f7704794786ccdedd3840d945e3ba8a
haroldwongms <harold.wong@microsoft.com>
2016-05-03 13:32:41 -0700

    Comment out Containerized variable
    Deploy a non containerized version.

--- deployOpenShift.sh
+++ deployOpenShift.sh
@@ -43,7 +43,7 @@ ansible_ssh_user=$SUDOUSER
 ansible_sudo=true
 deployment_type=origin
 docker_udev_workaround=True
-containerized=true
+# containerized=true
 openshift_use_dnsmasq=no
 
 openshift_master_cluster_public_hostname=$MASTERPUBLICIPHOSTNAME
