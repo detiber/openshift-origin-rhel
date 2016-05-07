19fd2f45d73a0de740243e637dd41337fecb2c6e
Jason DeTiberus <jdetiber@redhat.com>
2016-05-06 23:04:41 -0400

    some nodePrep cleanup/improvements

--- nodePrep.sh
+++ nodePrep.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
+set -eu
 
 # Update system to latest packages and install dependencies
 yum -y update
 yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion pyOpenSSL
-yum -y install https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-6.noarch.rpm
 
-sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
+# Install epel-release if not already installed
+rpm -q epel-release || yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+# ensure epel repo is disabled by default
+yum-config-manager epel --disable > /dev/null
 
 # Install Ansible
-
 yum -y --enablerepo=epel install ansible1.9
-# Install the epel repo if not already present
-# yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-# Clean yum metadata and cache to make sure we see the latest packages available
-# yum -y clean all
-# Install the ansible 1.9.4 package directly from koji
-# yum -y install https://kojipkgs.fedoraproject.org//packages/ansible/1.9.4/1.el7/noarch/ansible-1.9.4-1.el7.noarch.rpm
-# Disable EPEL to prevent unexpected packages from being pulled in during installation.
-#yum-config-manager epel --disable
-
 
 # Install Docker
 yum -y install docker
 
 # Create thin pool logical volume for Docker
-echo "DEVS=/dev/sdc" >> /etc/sysconfig/docker-storage-setup
-echo "VG=docker-vg" >> /etc/sysconfig/docker-storage-setup
-docker-storage-setup
+cat >>/etc/sysconfig/docker-storage-setup <<EOF
+DEVS=/dev/sdc
+VG=docker-vg
+EOF
 
 # Enable and start Docker services
 systemctl enable docker
